8a9e62c57d82e7bbfde3808e5078514dfc45f2ab
Dario Niero <dario.niero@enginius.com>
2023-05-04 21:51:59 +0200

    delete default page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LoginComponent } from './components/login/login.component';
-import { RegisterComponent } from './src/app/components/register/register.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { FirstComponent } from './components/first/first.component';
 import { SecondComponent } from './components/second/second.component';
 import { ThirdComponent } from './components/third/third.component';
+import { RegisterComponent } from './components/register/register.component';
 
 @NgModule({
   declarations: [
